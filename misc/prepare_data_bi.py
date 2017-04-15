45f311abb1d155adbfb16a615bd382ecbaf108f6
raphaelrubino <raphael.rubino@dfki.de>
2017-04-15 19:10:39 +0200

    Bilingual data preparation

--- misc/prepare_data_bi.py
+++ misc/prepare_data_bi.py
@@ -5,19 +5,46 @@ import sys
 from nltk.util import ngrams
 import numpy as np
 
+def format_line( line, padding = 1 ):
+	line = line.strip()
+	line = "<s> " * padding + line + " </s>" * padding
+	line = line.split( " " )
+	return line
+
+def init_vocab():
+	vocab = {
+		"unk": 0,
+		"<s>": 1,
+		"</s>": 2,
+	}
+
+def extract_vocabulary_and_sentences( in_corpus ):
+	corpus = []
+	vocab = init_vocab()
+	count = len( vocab )
+	with open( in_corpus, mode = "r" ) as c:
+		for line in c:
+			line = format_line( line, 1 )
+			numberized_tokens = []
+			for token in line:
+				if token not in vocab: 
+					vocab[ token ] = count
+					count += 1
+				numberized_tokens.append( vocab[ token ] )
+			corpus.append( numberized_tokens )
+	return vocab, corpus
+
 def extract_vocabulary_and_ngrams( in_corpus, n_order ):
 	corpus_contexts = []
 	corpus_targets = []
-	vocab = {}
-	vocab[ "unk" ] = 0
-	vocab[ "<s>" ] = 1
-	vocab[ "</s>" ] = 2
+	vocab = init_vocab()
 	count = len( vocab )
+	sentence_idx = []
+	count_sentence = -1
 	with open( in_corpus, mode = "r" ) as corpus:
 		for line in corpus:
-			line = line.strip()
-			line = "<s> " * ( n_order - 1 ) + line + " </s>" * ( n_order - 1 )
-			tokens = line.split( " " )
+			count_sentence += 1
+			tokens = format_line( line, ( n_order - 1 ) )
 			numberized_tokens = []
 			for token in tokens:
 				if token not in vocab:
@@ -31,28 +58,42 @@ def extract_vocabulary_and_ngrams( in_corpus, n_order ):
 				target = str( ngram[ n_order - 1 ] )
 				corpus_contexts.append( left_context + " " + right_context )
 				corpus_targets.append( target )
+				sentence_idx.append( count_sentence )
 	vocab = [ "{0} {1}".format( item, vocab[ item ] ) for item in vocab ]
-	return vocab, corpus_contexts, corpus_targets
+	return vocab, corpus_contexts, corpus_targets, sentence_idx
 	
+def multiply_sentences( sentences, idx ):
+	multi_sentences = []
+	for i in idx:
+		multi_sentences.append( sentences[ i ] )
+	return multi_sentences
+
 def write_file( towrite, out_file ):
 	with open( out_file, mode = "w" ) as out:
 		out.write( "\n".join( towrite ) )
 
 if __name__ == '__main__':
 
-	if len( sys.argv ) != 4:
-		print( "\nUsage: ", sys.argv[ 0 ], "<input corpus> <ngram order> <output prefix>\n" )
+	if len( sys.argv ) != 6:
+		print( "\nUsage: ", sys.argv[ 0 ], "<input src corpus> <input trg corpus> <trg ngram order> <src output prefix> <trg output prefix>\n" )
 		exit()
 
-	in_corpus, n_order, out_prefix = sys.argv[ 1: ]
+	in_src_corpus, in_trg_corpus, trg_n_order, out_src_prefix, out_trg_prefix = sys.argv[ 1: ]
 
-	n_order = np.int( n_order )
+	out_src_sentences = "{0}.sentences".format( out_src_prefix )
+	out_src_vocab = "{0}.vocab".format( out_src_prefix )
+	src_vocab, src_sentences = extract_vocabulary_and_sentences( in_src_corpus )
 
-	out_context = "{0}.context".format( out_prefix )
-	out_target = "{0}.target".format( out_prefix )
-	out_vocab = "{0}.vocab".format( out_prefix )
+	trg_n_order = np.int( trg_n_order )
+	out_trg_context = "{0}.context".format( out_trg_prefix )
+	out_trg_target = "{0}.target".format( out_trg_prefix )
+	out_trg_vocab = "{0}.vocab".format( out_trg_prefix )
+	trg_vocab, trg_contexts, trg_targets, trg_sentence_idx = extract_vocabulary_and_ngrams( in_trg_corpus, trg_n_order )
 
-	vocab, contexts, targets = extract_vocabulary_and_ngrams( in_corpus, n_order )
-	write_file( contexts, out_context )
-	write_file( targets, out_target )
-	write_file( vocab, out_vocab )
+	src_sentences = multiply_sentences( src_sentences, trg_sentence_idx )
+	
+	write_file( src_sentences, out_src_sentences )
+	write_file( src_vocab, out_src_vocab )
+	write_file( trg_contexts, out_trg_context )
+	write_file( trg_targets, out_trg_target )
+	write_file( trg_vocab, out_trg_vocab )
